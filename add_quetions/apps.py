1e09fc1cca1251a12d91a5327e3d646620cd25fb
Madiyar <madiarduysenbay@gmail.com>
2023-12-24 17:25:13 +0600

    merge commit

---
+++ add_quetions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AddQuetionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'add_quetions'
---
+++ add_quetions/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Subjects(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Category(models.Model):
+    name = models.CharField(max_length = 50)
+
+class Score(models.Model):
+    name = models.CharField(max_length = 30)
+    score = models.IntegerField()
+
+class Questions(models.Model):
+    subject = models.ForeignKey(Subjects, on_delete = models.CASCADE, related_name = 'subject')
+    category = models.ForeignKey(Category,on_delete = models.CASCADE, related_name = 'category')
+    question = models.JSONField()
+    answer = models.CharField(max_length = 50)
+    score = models.ForeignKey(Score, on_delete = models.CASCADE, related_name = 'score')
